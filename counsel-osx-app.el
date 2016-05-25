b04a8477bfd6f02eb8652d707a27c1a12ac8d878
d12frosted <d12frosted@users.noreply.github.com>
2016-05-25 09:19:38 +0300

    update prefixes

--- counsel-osx-app.el
+++ counsel-osx-app.el
@@ -20,17 +20,17 @@
 ;; one can select any file to edit in selected application via ivy actions.
 ;;
 ;; By default `counsel-osx-app' searches for applications in "/Applications"
-;; directory, but it's configurable via `counsel-apps-location' variable.  It can
+;; directory, but it's configurable via `counsel-osx-app-location' variable.  It can
 ;; be either string representing root location for all applications or list of
 ;; such strings.
 ;;
 ;; The last configurable thing (but not least) is command for launching
-;; application.  Please refer to `counsel-app-launch-cmd' for more information.
+;; application.  Please refer to `counsel-osx-app-launch-cmd' for more information.
 ;;
 ;; Although the name of this package is `counsel-osx-app', it's not restricted
 ;; to OSX only.  One can easily tune it to run under Linux (not sure about
 ;; Windows).  Just make sure to configure described variables and change
-;; implementation of `counsel-apps-list' function.  PRs are welcome on making
+;; implementation of `counsel-osx-app-list' function.  PRs are welcome on making
 ;; this package cross-platform.
 ;;
 ;;; Code:
@@ -38,16 +38,16 @@
 
 (require 'ivy)
 
-(defvar counsel-apps-location "/Applications"
+(defvar counsel-osx-app-location "/Applications"
   "Path to directory containing applications.")
 
-(defvar counsel-apps-pattern "*.app"
-  "Pattern for applications in `counsel-apps-location'.
+(defvar counsel-osx-app-pattern "*.app"
+  "Pattern for applications in `counsel-osx-app-location'.
 
-Use \"*\" if all files in `counsel-apps-location' are considered
+Use \"*\" if all files in `counsel-osx-app-location' are considered
 applications.")
 
-(defvar counsel-app-launch-cmd (lambda (app &optional file)
+(defvar counsel-osx-app-launch-cmd (lambda (app &optional file)
                                      (if (bound-and-true-p file)
                                          (format "open '%s' -a '%s'" file app)
                                        (format "open '%s'" app)))
@@ -57,30 +57,30 @@ Can be either format string or function that accepts path to
 application as first argument and filename as optional second
 argument and returns command.")
 
-(defun counsel-apps-list ()
-  "Get the list of applications under `counsel-apps-location'."
-  (let* ((locs (if (stringp counsel-apps-location)
-                   `(,counsel-apps-location)
-                 counsel-apps-location))
+(defun counsel-osx-app-list ()
+  "Get the list of applications under `counsel-osx-app-location'."
+  (let* ((locs (if (stringp counsel-osx-app-location)
+                   `(,counsel-osx-app-location)
+                 counsel-osx-app-location))
          (files (mapcar (lambda (path)
                           (file-expand-wildcards
-                           (concat path "/" counsel-apps-pattern)))
+                           (concat path "/" counsel-osx-app-pattern)))
                         locs)))
     (apply #'append files)))
 
-(defun counsel-app-action-default (app)
-  "Launch APP using `counsel-app-launch-cmd'."
+(defun counsel-osx-app-action-default (app)
+  "Launch APP using `counsel-osx-app-launch-cmd'."
   (call-process-shell-command
    (cond
-    ((stringp counsel-app-launch-cmd)
-     (format "%s '%s'" counsel-app-launch-cmd app))
-    ((functionp counsel-app-launch-cmd)
-     (funcall counsel-app-launch-cmd app))
+    ((stringp counsel-osx-app-launch-cmd)
+     (format "%s '%s'" counsel-osx-app-launch-cmd app))
+    ((functionp counsel-osx-app-launch-cmd)
+     (funcall counsel-osx-app-launch-cmd app))
     (t
-     (user-error "Could not construct cmd from `counsel-app-launch-cmd'")))))
+     (user-error "Could not construct cmd from `counsel-osx-app-launch-cmd'")))))
 
-(defun counsel-app-action-file (app)
-  "Open file in APP using `counsel-app-launch-cmd'."
+(defun counsel-osx-app-action-file (app)
+  "Open file in APP using `counsel-osx-app-launch-cmd'."
   (let* ((short-name (file-name-nondirectory app))
          (file (and short-name
                     (read-file-name
@@ -88,24 +88,24 @@ argument and returns command.")
     (if file
         (call-process-shell-command
          (cond
-          ((stringp counsel-app-launch-cmd)
-           (format "%s '%s' '%s'" counsel-app-launch-cmd app file))
-          ((functionp counsel-app-launch-cmd)
-           (funcall counsel-app-launch-cmd app file))
+          ((stringp counsel-osx-app-launch-cmd)
+           (format "%s '%s' '%s'" counsel-osx-app-launch-cmd app file))
+          ((functionp counsel-osx-app-launch-cmd)
+           (funcall counsel-osx-app-launch-cmd app file))
           (t
-           (user-error "Could not construct cmd from `counsel-app-launch-cmd'"))))
+           (user-error "Could not construct cmd from `counsel-osx-app-launch-cmd'"))))
       (user-error "Cancelled"))))
 
 (ivy-set-actions
- 'counsel-app
- '(("f" counsel-app-action-file "run on a file")))
+ 'counsel-osx-app
+ '(("f" counsel-osx-app-action-file "run on a file")))
 
 ;;;###autoload
 (defun counsel-osx-app ()
   "Launch an application via ivy interface."
   (interactive)
-  (ivy-read "Run application: " (counsel-apps-list)
-            :action #'counsel-app-action-default
+  (ivy-read "Run application: " (counsel-osx-app-list)
+            :action #'counsel-osx-app-action-default
             :caller 'counsel-app))
 
 (provide 'counsel-osx-app)
