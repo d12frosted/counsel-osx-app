e794630989c8df75f6c025846d73d911cc63cbd6
d12frosted <d12frosted@users.noreply.github.com>
2016-05-25 13:05:57 +0300

    change minimal Emacs version required
    Because `user-error` was added in `24.3`.
    
    https://github.com/melpa/melpa/pull/3929#issuecomment-221529402

--- counsel-osx-app.el
+++ counsel-osx-app.el
@@ -5,7 +5,7 @@
 ;; Author: Boris Buliga <d12frosted@gmail.com>
 ;; URL: https://github.com/d12frosted/counsel-osx-app
 ;; Version: 0.1.0
-;; Package-Requires: ((ivy "0.8.0") (emacs "24.1"))
+;; Package-Requires: ((ivy "0.8.0") (emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
